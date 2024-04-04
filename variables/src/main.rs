d8fc5eae8d4e7a5dc38d47f72936bda91d8f3de4
josevictormoreno <josevictor.admoreno@gmail.com>
2024-04-04 10:28:26 -0300

    feat: add chapter 3 variables, data types content
    this commit finishes the 2 first parts of chapter 3 from the rust
    programming language book

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,38 @@
+//fn main() {
+//// code 1
+//let x = 5;
+//let x = x + 1;
+//{
+//let x = x * 2;
+//println!("The value of x is: {x}");
+//}
+
+//println!("The value of x is: {x}");
+
+////code 2
+//let spaces = "   ";
+//let spaces = spaces.len();
+//}
+
+// code 3
+use std::io;
+
 fn main() {
-    let x = 5;
-    println!("The value of x is : {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index.trim().parse().expect("Index must be a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
