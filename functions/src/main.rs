0823fb46d8d2377a9975e59a6a4a02b8ddb10a4d
josevictormoreno <josevictor.admoreno@gmail.com>
2024-04-06 23:56:22 -0300

    feat: add functions with return value
    this commit add the new type of functoins with type value return, as a
    block

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,18 @@
-fn main() {
-    another_function(5);
+// code 1
+//fn main() {
+    //another_function(5);
+//}
+
+//fn another_function(x: i32) {
+    //println!("The value of x is: {}", x);
+//}
+
+// code 2
+fn five() -> i32 {
+    return 5;
 }
 
-fn another_function(x: i32) {
+fn main() {
+    let x = five();
     println!("The value of x is: {}", x);
 }
