f380baced294975b52ec93a53cfe937c04778ab0
josevictormoreno <josevictor.admoreno@gmail.com>
2024-04-13 20:26:24 -0300

    feat: add the part 2 code from references
    this commit introduces the part 2 of code from the book the rust
    programming language, we are currently on chapter 4

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,9 +1,20 @@
+// code 1
+//fn main() {
+    //let s1 = String::from("josevictorandreidaltomoreno");
+    //let len = calculate_length(&s1);
+    //println!("The length of '{}' is {}.", s1, len);
+//}
+
+//fn calculate_length(s: &String) -> usize {
+    //s.len()
+//}
+//
+// code 2
 fn main() {
-    let s1 = String::from("josevictorandreidaltomoreno");
-    let len = calculate_length(&s1);
-    println!("The length of '{}' is {}.", s1, len);
+    let s = String::from("José Victor Moreno");
+    change(&s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &String) {
+    some_string.push_str(", José Victor Moreno");
 }
