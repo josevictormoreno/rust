c7ef8fc7a921c1a37e4c186c304e90658e0bb2fe
josevictormoreno <josevictor.admoreno@gmail.com>
2024-04-16 21:45:29 -0300

    feat: add the last part of the references code
    this commit introduces the last part of the references of the rust
    programming language book, chapter 4.2

--- references/src/main.rs
+++ references/src/main.rs
@@ -8,13 +8,27 @@
 //fn calculate_length(s: &String) -> usize {
     //s.len()
 //}
-//
-// code 2
-fn main() {
-    let s = String::from("José Victor Moreno");
-    change(&s);
-}
 
-fn change(some_string: &String) {
-    some_string.push_str(", José Victor Moreno");
+// code 2
+//fn main() {
+    //let mut s = String::from("José Victor Moreno");
+    //change(&mut s);
+//}
+
+//fn change(some_string: &mut String) {
+    //some_string.push_str(", José Victor Moreno");
+//}
+
+// code 3
+
+fn main () {
+    let s = String::from("hello");
+
+    // we can do this
+    let r1 = &s;
+    let r2 = &s;
+
+    //we cannot do this
+    //because the last two references are not expecting the value to change
+    let r3 = &mut s;
 }
